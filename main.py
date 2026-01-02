2d8ffec71e548717351b3c39846b474041ad98e6
Steve-pro-10 <cadoste@outlook.it>
2026-01-02 14:01:05 +0100

    Added help parameter and command list

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ art = r"""
 """
 
 cm_list = """
-DoS attack [1]
+COMMAND LIST:
+DoS attack [1] 
 
+TYPE "[COMMAND NUMBER] help" to view the parameters of the command
 """
+help = {
+
+   1 : "DDOS ATTACK:\n Syntax: 1 [number of pings] [target (ip address of domain)]\n Es. 1 6 google.com" 
+}
+
+print(cm_list)
 class Tool():
     def __init__(self):
         pass
@@ -16,9 +24,13 @@ class Tool():
         console = input("write the number of the tool $ ")
         console = console.split()
         comm = console[0]
-        if int(comm) == 1: 
-            nping,target = console[1] , console[2]
-            ddos.Attack(nping,target).ping()
+        #DDOS ATTACK
+        if comm == "1" and console[1] == "help": print(help[1])
+        else: 
+            try:
+                nping,target = console[1] , console[2]
+                ddos.Attack(nping,target).ping()
+            except IndexError: print("Error: Missing parameters")
 
 if __name__ == "__main__":
     app = Tool()
